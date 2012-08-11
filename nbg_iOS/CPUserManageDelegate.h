26518e6866fdb190e74eb925bbfa75971748e1f9
ZeR0-Wu <thelongestmailaccounteverexist@gmail.com>
2012-08-12 01:14:12 +0800

    commit today. many undone

---
+++ nbg_iOS/CPUserManageDelegate.h
@@ -0,0 +1,13 @@
+//
+//  CPAccountManageDelegate.h
+//  nbg_iOS
+//
+//  Created by wuhaotian on 12-8-11.
+//  Copyright (c) 2012年 wuhaotian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol CPAccountManageDelegate <NSObject>
+
+@end
